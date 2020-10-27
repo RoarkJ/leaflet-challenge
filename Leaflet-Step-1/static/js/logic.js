889c8208dfacf871ce005f7f237a6e962679bcbc
Bryan <30875957+RoarkJ@users.noreply.github.com>
2020-10-27 13:02:52 -0600

    comment out parts that go with step 2

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -7,23 +7,23 @@ var greyscale = L.tileLayer("https://api.tiles.mapbox.com/styles/v1/{id}/tiles/{
   id: "mapbox/light-v10",
   accessToken: API_KEY
 });
-var satelite_map = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    mapZoom: 15,
-    zoomOffset: -1,
-    id: "mapbox/satellite-v9",
-    accessToken: API_KEY
-});
+// var satelite_map = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//     tileSize: 512,
+//     mapZoom: 15,
+//     zoomOffset: -1,
+//     id: "mapbox/satellite-v9",
+//     accessToken: API_KEY
+// });
     
-var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    mapZoom: 15,
-    zoomOffset: -1,
-    id: "mapbox/outdoors-v11",
-    accessToken: API_KEY
-});
+// var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//     tileSize: 512,
+//     mapZoom: 15,
+//     zoomOffset: -1,
+//     id: "mapbox/outdoors-v11",
+//     accessToken: API_KEY
+// });
 // Create our map, giving it the streetmap and earthquakes layers to display on load
 var myMap = L.map("mapid", {
   center: [
@@ -34,19 +34,19 @@ var myMap = L.map("mapid", {
 greyscale.addTo(myMap);
 
 //Base Map
-var baseMaps = {
-    Greyscale: greyscale,
-    Satellite: satelite_map,
-    Outdoor: outdoors
-};
+// var baseMaps = {
+//     Greyscale: greyscale,
+//     Satellite: satelite_map,
+//     Outdoor: outdoors
+// };
 // Overlays Maps
 var earthquakes = new L.LayerGroup();
-var overlays = {
-   // "Techtonic Plates": techtonicplates, ( not adding to assignment)
-    "Earthquakes": earthquakes
-};
+// var overlays = {
+//    // "Techtonic Plates": techtonicplates, ( not adding to assignment)
+//     "Earthquakes": earthquakes
+// };
 //Add Control Layer
-L.control.layers(baseMaps, overlays).addTo(myMap);
+// L.control.layers(baseMaps, overlays).addTo(myMap);
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
@@ -75,7 +75,7 @@ function circleMakers(feature) {
     fillOpacity: 0.8
     };
 }
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Create a GeoJSON layer containing the desired features on the earthquakeData object
    L.geoJSON(data, {
     pointToLayer: function(feature, latlong){
       return L.circleMarker(latlong);
